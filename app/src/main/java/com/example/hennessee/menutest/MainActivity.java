86ba10e76541ce99d214d937ab0b716ee6479796
Vasir1 <fullmetilalchemist@gmail.com>
2014-07-30 13:38:34 -0400

    Added scan

--- app/src/main/java/com/example/hennessee/menutest/MainActivity.java
+++ app/src/main/java/com/example/hennessee/menutest/MainActivity.java
@@ -1,27 +1,21 @@
 package com.example.hennessee.menutest;
 
-import android.app.Activity;
-import android.net.Uri;
-
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 
 public class MainActivity extends Activity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks, SpecialsFragment.OnFragmentInteractionListener, DessertsFragment.OnFragmentInteractionListener, AppetizersFragment.OnFragmentInteractionListener {
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks,scanFragment.OnFragmentInteractionListener, SpecialsFragment.OnFragmentInteractionListener, DessertsFragment.OnFragmentInteractionListener, AppetizersFragment.OnFragmentInteractionListener {
 
 
     public void onFragmentInteraction(Uri uri)
@@ -61,12 +55,17 @@ public class MainActivity extends Activity
         switch (position)
         {
             case 0:
-                fragment = new SpecialsFragment();
+                fragment = new scanFragment();
                 break;
             case 1:
-                fragment = new AppetizersFragment();
+                fragment = new SpecialsFragment();
+
                 break;
             case 2:
+                fragment = new AppetizersFragment();
+
+                break;
+            case 3:
                 fragment = new DessertsFragment();
                 break;
             default:
---
+++ app/src/main/java/com/example/hennessee/menutest/scanFragment.java
@@ -0,0 +1,195 @@
+package com.example.hennessee.menutest;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Kyle on 29/07/2014.
+ */
+public class scanFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SpecialsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static scanFragment newInstance(String param1, String param2) {
+        scanFragment fragment = new scanFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    //QR code app calls this after it scans a QR code
+
+    /*
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+
+
+                String contents = intent.getStringExtra("SCAN_RESULT"); // This will contain your scan result
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+
+
+                //Creating toast notification to display what the scan found.
+                Toast.makeText(getActivity(), contents,
+                        Toast.LENGTH_SHORT).show();
+
+                try {
+                    Socket s = new Socket("localhost",12345); // Change localhost to your IP, obviously.
+                    //Could even get IP from the scan.
+
+                    //outgoing stream redirect to socket
+                    OutputStream out = s.getOutputStream();
+
+                    PrintWriter output = new PrintWriter(out);
+                    output.println("Hello Android!");
+                    BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+                    //read line(s)
+                    String st = input.readLine();
+                    ///. . .
+                    //Close connection
+                    s.close();
+
+
+                } catch (UnknownHostException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+
+
+
+            }
+        }
+
+    }
+*/
+
+// ZXing Result Handler
+
+
+
+
+    public scanFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_scan, container, false);
+
+        final Button button = (Button) rootView. findViewById(R.id.buttonScan);
+        button.setOnClickListener(new View.OnClickListener() {
+            //  button.setOnClickListener(new OnClickListener() {
+
+            //Handles scan button click, makes a call to another QR scanning app
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(
+                        "com.google.zxing.client.android.SCAN");
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                getActivity().startActivityForResult(intent, 0);
+            }
+
+        });
+
+        return rootView;
+    }
+
+    // ZXing Result Handler
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+
+
+                String contents = intent.getStringExtra("SCAN_RESULT"); // This will contain your scan result
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+
+
+            }
+        }
+
+    }
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
